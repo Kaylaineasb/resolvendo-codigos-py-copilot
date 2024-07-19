bfb9506edfb9f8f30286f6015a5c815377cdf4c7
Kaylaine Assunção <85030686+Kaylaineasb@users.noreply.github.com>
2024-07-19 07:28:36 -0300

    Update ver.Numbers.py

--- resolucoes_code/ver.Numbers.py
+++ resolucoes_code/ver.Numbers.py
@@ -1,1 +1,10 @@
-#verificar se o número é par ou ìmpar
+#verificar se o número é par ou ìmpar#
+# Recebendo um número inteiro do usuário
+numero = int(input("Digite um número inteiro: "))
+
+# Verificando se o número é par ou ímpar
+if numero % 2 == 0:
+    print(f"O número {numero} é par.")
+else:
+    print(f"O número {numero} é ímpar.")
+
